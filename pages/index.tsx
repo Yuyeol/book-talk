48e9b0e20f6a977c9f16d5ab2be28695741aa757
ure <uy23@naver.com>
2023-04-08 05:49:55 +0900

    feat #3: floating button 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import BookItem from "@/components/book/item";
-import FloatingButton from "@/components/floating-button";
 import Plus from "@/components/icon/plus";
 import Layout from "@/components/layout";
 
@@ -10,10 +9,6 @@ const Home = () => {
         <BookItem />
         <BookItem />
       </ul>
-
-      <FloatingButton href="/book/1">
-        <Plus width={8} color="white" />
-      </FloatingButton>
     </Layout>
   );
 };
