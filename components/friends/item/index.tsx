6ab239c7dea05fead7797dcbd56fe33f12cdcc1b
ure <uy23@naver.com>
2023-04-09 01:38:44 +0900

    feat #3: friends 초안 완료

---
+++ components/friends/item/index.tsx
@@ -0,0 +1,56 @@
+import Chevron from "@/components/icon/chevron";
+import { useState } from "react";
+
+const BookInfo = ({ type }: { type: 1 | 2 | 3 }) => {
+  return (
+    <div className="flex gap-2 mb-3 text-xs">
+      <div>
+        {type === 1 && "읽고 있는 책"}
+        {type === 2 && "함께 읽는 책"}
+        {type === 3 && "아카이브"}
+      </div>
+      <div className="w-10 h-10 bg-slate-200">사진</div>
+      <div className="flex flex-col items-center h-10">
+        <div className="">책제목</div>
+        <div className="px-1 my-auto rounded-full bg-slate-400">+9</div>
+      </div>
+    </div>
+  );
+};
+
+const FriendsItem = () => {
+  const [isSelected, setIsSelected] = useState(false);
+  return (
+    <li
+      className="overflow-hidden"
+      onClick={() => setIsSelected((prev) => !prev)}
+    >
+      <div className="relative z-10 flex items-center py-2 bg-white">
+        <div className="w-12 h-12 mr-2 rounded-md bg-slate-600">사진</div>
+        <div className="flex-1">
+          <div className="">이름</div>
+          <div className="text-sm">상태메시지</div>
+        </div>
+        <div
+          className={`${
+            isSelected ? "rotate-180" : "rotate-0"
+          } transition-transform duration-500`}
+        >
+          <Chevron width={5} color="black" />
+        </div>
+      </div>
+      <div
+        className={`z-0 -top-full ${
+          isSelected ? "mt-0" : "-mt-[50%]"
+        } transition-[margin-top] duration-500 ease-in-out`}
+      >
+        <div className="">
+          <BookInfo type={1} />
+          <BookInfo type={2} />
+          <BookInfo type={3} />
+        </div>
+      </div>
+    </li>
+  );
+};
+export default FriendsItem;
--- pages/Friends.tsx
+++ pages/Friends.tsx
@@ -1,14 +1,14 @@
+import FriendsItem from "@/components/friends/item";
 import Layout from "@/components/layout";
 
 const Friends = () => {
   return (
     <Layout>
-      <ul>
+      <ul className="px-4 divide-y-2">
         {/* 클릭 시 상세 정보 모달: 함께 읽는 책, 이사람이 읽는책, 친구삭제 */}
-        <li>
-          <div>사진</div>
-          <div>이름, 상태메시지</div>
-        </li>
+        <FriendsItem />
+        <FriendsItem />
+        <FriendsItem />
       </ul>
     </Layout>
   );
