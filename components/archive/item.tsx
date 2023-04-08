fc6f39195e8d69a428784db271919c07bc6d0a64
ure <uy23@naver.com>
2023-04-08 22:13:17 +0900

    feat #3: archive font, image 수정

--- components/archive/item.tsx
+++ components/archive/item.tsx
@@ -3,8 +3,13 @@ import ResponsiveImage from "../core/responsive-image";
 const ArchiveItem = () => {
   return (
     <li>
-      <ResponsiveImage src="" alt="" aspectRatio="1" />
-      <div className="mt-1">book name</div>
+      <ResponsiveImage
+        src="/mock/book2.jpeg"
+        alt=""
+        aspectRatio="1"
+        objectFit="contain"
+      />
+      <div className="mt-1 text-center">book name</div>
     </li>
   );
 };
