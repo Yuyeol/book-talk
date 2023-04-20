2c2f86bf08c3d79f735d5dd3ae331236fa692c76
ure <uy23@naver.com>
2023-04-20 22:21:50 +0900

    feat #7: elapsed time 구현

--- components/book/book-item.tsx
+++ components/book/book-item.tsx
@@ -1,13 +1,26 @@
-const BookItem = () => {
+import { getElapsedTime } from "@/lib/client/elapsedTime";
+import { Book } from "@prisma/client";
+import dayjs from "dayjs";
+import { useMemo } from "react";
+
+interface Props {
+  book: Book;
+}
+
+const BookItem = ({ book }: Props) => {
   return (
     <li className="flex items-center py-2">
       <div className="w-12 h-12 mr-2 rounded-md bg-slate-600">사진</div>
       <div className="flex-1">
-        <div className="">책 제목</div>
-        <div className="text-sm">설명</div>
+        <div className="">{book.title}</div>
+        <div className="text-sm">{book.description}</div>
       </div>
       <div className="w-[70px]">
-        <div className="mx-auto mb-1 text-xs text-center">2022.02.21</div>
+        <div className="mx-auto mb-1 text-xs text-center">
+          {getElapsedTime(
+            book.updatedAt ? dayjs(book.updatedAt) : dayjs(book.createdAt)
+          )}
+        </div>
         <div className="flex items-center justify-center w-5 h-5 mx-auto text-xs text-center text-white rounded-full bg-slate-600">
           1
         </div>
