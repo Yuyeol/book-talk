c12d76e306fc958d8fbead584554126d2ec1dd9d
ure <uy23@naver.com>
2023-05-05 14:44:29 +0900

    feat #7: backBtn 뒤로가기 추가

--- components/header/title-col.tsx
+++ components/header/title-col.tsx
@@ -1,6 +1,7 @@
 import { HEADER_ICON_COLOR } from "@/constants";
 import ChevronLeft from "../icon/chevron-left";
 import { HEADER_ICON_WIDTH } from "@/constants";
+import { useRouter } from "next/router";
 
 interface IProps {
   children: React.ReactNode;
@@ -8,10 +9,13 @@ interface IProps {
 }
 
 const TitleCol = ({ children, hasBackBtn }: IProps) => {
+  const router = useRouter();
   return (
     <div className="flex items-center gap-1">
       {hasBackBtn && (
-        <ChevronLeft width={HEADER_ICON_WIDTH} color={HEADER_ICON_COLOR} />
+        <button onClick={() => router.back()}>
+          <ChevronLeft width={HEADER_ICON_WIDTH} color={HEADER_ICON_COLOR} />
+        </button>
       )}
 
       <div className="text-xl font-bold text">{children}</div>
