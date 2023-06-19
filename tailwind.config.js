396ab04ea08e5323aa1e9414484c46c02f8c56da
ure <uy23@naver.com>
2023-06-19 16:22:45 +0900

    feat #24: tag color, theme color 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-black": "#0D0D0D",
+        "soft-white": "F2F2F2",
+        "primary-green": "#5DB075",
+        "sub-green": "#4B9460",
+        "grey-100": "#F6F6F6",
+        "grey-200": "#E8E8E8",
+        "grey-300": "#BDBDBD",
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
