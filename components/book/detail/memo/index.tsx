cd0dd571ace2ee2c31c614cac66399a6e518c00e
ure <uy23@naver.com>
2023-06-22 19:18:34 +0900

    feat #24: memo: more 버튼 추가

--- components/book/detail/memo/index.tsx
+++ components/book/detail/memo/index.tsx
@@ -1,13 +1,9 @@
 import { getElapsedTime } from "@/lib/client/getElapsedTime";
-import useMutation from "@/lib/client/useMutation";
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 import BookInfo from "./book-info";
-import useMemos from "@/lib/client/useSwr/useMemos";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { Memo } from "@prisma/client";
 import Reaction from "@/components/book/detail/memo/reaction";
+import More from "@/components/book/detail/memo/more";
 
 interface IProps {
   memo: Memo;
@@ -15,33 +11,10 @@ interface IProps {
 }
 
 const Memo = ({ memo, isSocialFeed }: IProps) => {
-  const {
-    query: { bookId },
-  } = useRouter();
   const { data: session } = useSession();
-  const {
-    mutation,
-    loading,
-    data: memoResData,
-  } = useMutation(`/api/memos/${memo.id}`);
-  const { data: memosData, mutate } = useMemos(parseInt(bookId as string));
 
   // 해당 메모의 작성자인지 확인
   const isOwner = memo.userId === session?.user?.id;
-
-  const onDeleteMemo = (id: number) => {
-    if (loading) return;
-    mutation({ id: id }, "DELETE");
-  };
-  useEffect(() => {
-    if (memoResData && memosData?.ok) {
-      mutate({
-        ok: true,
-        memos: memosData.memos.filter((m) => m.id !== memo.id),
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [memoResData]);
 
   return (
     <div className="p-4">
@@ -49,30 +22,15 @@ const Memo = ({ memo, isSocialFeed }: IProps) => {
         {isSocialFeed && <BookInfo memo={memo} />}
         <div className="flex items-center justify-between text-xs pb-2 mb-1">
           <div className="">Page. {memo.page}</div>
-          <div className="text-xs">
-            {getElapsedTime(memo.updatedAt) ?? getElapsedTime(memo.createdAt)}
-          </div>
+          {isOwner && !isSocialFeed && (
+            <More memoId={memo.id} bookId={memo.bookId} />
+          )}
         </div>
-        <div className="text-sm border-b-[1px] border-primary-green/30 pb-2 mb-2">
-          {memo.content}
+        <div className="text-sm">{memo.content}</div>
+        <div className="text-xs text-end border-b-[1px] border-primary-green/30 pb-2 mb-2">
+          {getElapsedTime(memo.updatedAt) ?? getElapsedTime(memo.createdAt)}
         </div>
         <Reaction memoId={memo.id} />
-        {isOwner && (
-          <div className="flex justify-end gap-1">
-            <Link
-              href={`/books/${memo.bookId}/memo/${memo.id}/edit`}
-              className="c_button_underlined"
-            >
-              수정
-            </Link>
-            <button
-              className="c_button_underlined"
-              onClick={() => onDeleteMemo(memo.id)}
-            >
-              삭제
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
---
+++ components/book/detail/memo/more.tsx
@@ -0,0 +1,83 @@
+import useMutation from "@/lib/client/useMutation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Dots from "@/components/icon/dots";
+import { PRIMARY_GREEN, SOFT_BLACK } from "@/constants";
+import useMemos from "@/lib/client/useSwr/useMemos";
+import Link from "next/link";
+
+interface IProps {
+  memoId: number;
+  bookId: number;
+}
+
+const More = ({ memoId, bookId }: IProps) => {
+  const router = useRouter();
+  const [isMoreOpened, setIsMoreOpened] = useState(false);
+
+  const toggleButtons = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsMoreOpened(!isMoreOpened);
+  };
+  const {
+    mutation,
+    loading,
+    data: memoResData,
+  } = useMutation(`/api/memos/${memoId}`);
+  const { data: memosData, mutate } = useMemos(
+    parseInt(router.query.bookId as string)
+  );
+
+  const onDeleteMemo = (id: number) => {
+    if (loading) return;
+    mutation({ id: id }, "DELETE");
+  };
+
+  useEffect(() => {
+    if (memoResData && memosData?.ok) {
+      mutate({
+        ok: true,
+        memos: memosData.memos.filter((m) => m.id !== memoId),
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [memoResData]);
+
+  return (
+    <div className="space-x-1 flex items-center">
+      <div className="overflow-hidden px-2 translate-x-2">
+        <div
+          className={`transition-all duration-300 ease-out space-x-1 ${
+            isMoreOpened
+              ? "translate-x-0 opacity-100"
+              : "translate-x-2 opacity-0"
+          }`}
+        >
+          <Link href={`/books/${bookId}/memo/${memoId}/edit`}>
+            <button className="c_button_block_xs">수정</button>
+          </Link>
+          <button
+            className="c_button_block_xs"
+            onClick={() => onDeleteMemo(memoId)}
+          >
+            삭제
+          </button>
+        </div>
+      </div>
+      <button
+        className={`c_button_underlined inline-block transition-transform duration-300 ease-out ${
+          isMoreOpened ? "rotate-0" : "rotate-90"
+        }`}
+        onClick={(e) => toggleButtons(e)}
+      >
+        <div>
+          <Dots
+            width={1.25}
+            color={isMoreOpened ? PRIMARY_GREEN : SOFT_BLACK}
+          />
+        </div>
+      </button>
+    </div>
+  );
+};
+export default More;
